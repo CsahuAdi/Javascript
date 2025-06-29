1008f5f28198bdc3ff9829f0b231cc7f69b6ab37
CsahuAdi <chiranjancs01032006@gmail.com>
2025-06-29 23:52:51 +0530

    Auto reset after win

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded',  () => {
-    console.log("Welcome to tic tac toe")
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to tic tac toe");
     let intro = new Audio("elements/game-start-317318.mp3");
     let changeTurn_sfx = new Audio("elements/click-tap-computer-mouse-352734.mp3");
     let gameOver_sfx = new Audio("elements/game-bonus-144751.mp3");
@@ -8,11 +8,21 @@ document.addEventListener('DOMContentLoaded',  () => {
     let gameOver = false;
 
     const changeTurn = () => {
-        return turn === 'X'?'O': 'X';
+        return turn === 'X' ? 'O' : 'X';
+    }
+
+    const resetGame = () => {
+        const boxtextList = document.getElementsByClassName('boxtext');
+        Array.from(boxtextList).forEach(boxtext => {
+            boxtext.innerText = '';
+        });
+        gameOver = false;
+        turn = 'X';
+        document.getElementsByClassName("info")[0].innerText = 'Turn for ' + turn;
     }
 
     const over = () => {
-        wins = [
+        const wins = [
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
@@ -22,42 +32,42 @@ document.addEventListener('DOMContentLoaded',  () => {
             [0, 4, 8],
             [2, 4, 6]
         ];
-        boxtextList = document.getElementsByClassName('boxtext');
+        const boxtextList = document.getElementsByClassName('boxtext');
         wins.forEach(e => {
-            if((boxtextList[e[0]].innerText !== '') && (boxtextList[e[0]].innerText === boxtextList[e[1]].innerText) && (boxtextList[e[1]].innerText === boxtextList[e[2]].innerText)) {
-                document.querySelector('.info').innerText = boxtextList[e[0]].innerText + "Won";
+            if ((boxtextList[e[0]].innerText !== '') &&
+                (boxtextList[e[0]].innerText === boxtextList[e[1]].innerText) &&
+                (boxtextList[e[1]].innerText === boxtextList[e[2]].innerText)) {
+
+                document.querySelector('.info').innerText = boxtextList[e[0]].innerText + " Won";
                 gameOver = true;
+                gameOver_sfx.play();
+
+                // Auto-reset after 2 seconds
+                setTimeout(() => {
+                    resetGame();
+                }, 2000);
             }
-        })
+        });
     }
 
-    let boxes = document.getElementsByClassName("box");
+    const boxes = document.getElementsByClassName("box");
     Array.from(boxes).forEach(element => {
-        let boxtext = element.querySelector('.boxtext');
+        const boxtext = element.querySelector('.boxtext');
         element.addEventListener('click', () => {
-            if(boxtext.innerText === '') {
+            if (boxtext.innerText === '' && !gameOver) {
                 boxtext.innerText = turn;
-                turn = changeTurn();
                 changeTurn_sfx.play();
                 over();
-                if(!gameOver) {
+                if (!gameOver) {
+                    turn = changeTurn();
                     document.getElementsByClassName("info")[0].innerText = 'Turn for ' + turn;
-                }
-                if(gameOver) {
-                    gameOver_sfx.play();
                 }
             }
         });
     });
 
-    resetBtn = document.getElementById('reset');
+    const resetBtn = document.getElementById('reset');
     resetBtn.addEventListener('click', () => {
-        boxtextList = document.getElementsByClassName('boxtext');
-        Array.from(boxtextList).forEach(boxtext => {
-            boxtext.innerText = '';
-            gameOver =  false;
-            turn = 'X';
-            document.getElementsByClassName("info")[0].innerText = 'Turn for ' + turn;
-        });
+        resetGame();
     });
 });
